058f1e645d0308369977683ff408245152d349e4
Kate <kathii.conii@gmail.com>
2026-03-05 16:56:39 -0600

    Creación del proyecto y solución del laboratorio 1

---
+++ Laboratorio1_A85112/Models/ErrorViewModel.cs
@@ -0,0 +1,9 @@
+namespace Laboratorio1_A85112.Models
+{
+    public class ErrorViewModel
+    {
+        public string? RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
