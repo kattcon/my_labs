4c3edb214e0ebeb536a7562f52705b1115a9d428
Kate <kathii.conii@gmail.com>
2026-03-25 09:55:01 -0600

    laboratorio 4 archivos iniciales

---
+++ mi primer proyecto/backend-lab-A85112/backend-lab-A85112/Controllers/CountryController.cs
@@ -0,0 +1,16 @@
+﻿using Microsoft.AspNetCore.Http;
+using Microsoft.AspNetCore.Mvc;
+
+namespace backend_lab_A85112.Controllers
+{
+    [Route("api/[controller]")]
+    [ApiController]
+    public class CountryController : ControllerBase
+    {
+        [HttpGet]
+        public string Get()
+        {
+            return "Hola Mundo";
+        }
+    }
+}
