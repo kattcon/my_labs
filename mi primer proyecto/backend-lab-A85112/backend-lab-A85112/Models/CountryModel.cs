3bb0b67af47431afb151c62380206c7fc948925b
Kate <kathii.conii@gmail.com>
2026-03-26 22:19:16 -0600

    se arregla but, laboratorio 4

--- mi primer proyecto/backend-lab-A85112/backend-lab-A85112/Models/CountryModel.cs
+++ mi primer proyecto/backend-lab-A85112/backend-lab-A85112/Models/CountryModel.cs
@@ -4,7 +4,7 @@
     {
         public int Id { get; set; }
         public string Name { get; set; }
-        public string Continet { get; set; }
+        public string Continent { get; set; }
         public string Language { get; set; }
     }
 }
